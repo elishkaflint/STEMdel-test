71e3126ab7946c9693012cd8dcb94d7ce4d498b8
Elishka Flint <eflint@deloitte.co.uk>
2018-11-17 15:53:22 +0000

    Remove prior migrations

--- db/migrate/20181117132221_amend_films_table.rb
+++
@@ -1,8 +0,0 @@
-class AmendFilmsTable < ActiveRecord::Migration[5.2]
-  def change
-    change_table :films do |t|
-      t.remove :rating
-      t.boolean :pass_stemdel_test
-    end
-  end
-end
--- db/migrate/20181117132557_drop_films.rb
+++
@@ -1,5 +0,0 @@
-class DropFilms < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :films
-  end
-end
